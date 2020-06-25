18551863d7c717632074a2d9d0183b886869d453
NineBall <NineBallCode@gmail.com>
2020-06-25 17:54:46 -0600

    First commit

---
+++ GetString.cpp
@@ -0,0 +1,21 @@
+//
+// Created by nate on 2020-06-25.
+//
+#include "main.h"
+
+std::string GetStdoutFromCommand(std::string cmd) {
+
+    std::string data;
+    FILE * stream;
+    const int max_buffer = 256;
+    char buffer[max_buffer];
+    cmd.append(" 2>&1");
+
+    stream = popen(cmd.c_str(), "r");
+    if (stream) {
+        while (!feof(stream))
+            if (fgets(buffer, max_buffer, stream) != NULL) data.append(buffer);
+        pclose(stream);
+    }
+    return data;
+}
