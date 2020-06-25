aa1e5d7832710430d6b0603c0fefa8103c3d2166
Nate Dean <nkdean@icloud.com>
2020-06-25 17:54:46 -0600

    First commit

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include <boost/algorithm/string.hpp>
+#include <vector>
+#include "main.h"
+#include "regex"
+#include <unistd.h>
+#include "stdlib.h"
+
+int main(int argc, char** argv) {
+
+    //----------------------------------------------------------------------------//
+    //If command is passed as value use that, then run it.
+    std::string command;
+    if (argv[2]){
+        command = argv[2];
+        command + " &";
+    } else{
+        command = argv[1];
+        command + " &";
+    }
+    system("spotify &");
+    //----------------------------------------------------------------------------//
+
+
+
+    //loop every X ms until window is found and minimized//
+    int stopLoop = 0;
+    int loops = 0;
+    int loopsMax = 0;
+    int loopTime = 0;
+    argv[3] ? loopsMax = std::stoi(argv[3]) : loopsMax = 100;
+    argv[4] ? loopTime = std::stoi(argv[4]) * 100 : loopTime = 100000;
+    //----------------------------------------------------------------------------//
+    while (stopLoop == 0){
+        //----------------------------------------------------------------------------//
+        // Split by line into array
+        std::string openWindows = GetStdoutFromCommand("wmctrl -l");
+        std::string program = argv[1];
+        std::string minimize;
+        std::vector<std::string> strs;
+        boost::split(strs,openWindows,boost::is_any_of("\n"));
+        //----------------------------------------------------------------------------//
+
+        //----------------------------------------------------------------------------//
+        //loop through each string in array checking for argv[1]/window name
+        for (int i = 0; i < strs.size(); i++){
+            if (strs[i].find(program) != std::string::npos)
+            {
+                //----------------------------------------------------------------------------//
+                //Regex search
+                std::regex rgx ("^.{10}");
+                std::smatch matches;
+                if(std::regex_search(strs[i], matches, rgx)) {
+                //----------------------------------------------------------------------------//
+                    std::cout << "Match found\n";
+                    //----------------------------------------------------------------------------//
+                    // Minimalize program then stop loop
+                    minimize = "xdotool windowminimize " + matches[0].str();
+                    std::cout << minimize << "\n";
+                    system(minimize.c_str());
+                    stopLoop = 1;
+                    //----------------------------------------------------------------------------//
+                }
+                else {
+                std::cout << "Match not found\n";
+                }
+            }
+            else {
+                std::cout << "Match not found\n";
+            }
+        }
+        //----------------------------------------------------------------------------//
+
+
+        //----------------------------------------------------------------------------//
+        //Ok this window dosen't exist, closing program to save resorces. Add to loop counter and sleep for x
+        if (loops > loopsMax) {
+            std::cout << "Window not found, loops compleated: " << loops;
+            stopLoop = 1;
+        }
+
+        loops++;
+        usleep(loopTime);
+        //----------------------------------------------------------------------------//
+    }
+    return 0;
+}
+
---
+++ main.h
@@ -0,0 +1,11 @@
+//
+// Created by nate on 2020-06-25.
+//
+
+#ifndef MINIMIZE_MAIN_H
+#define MINIMIZE_MAIN_H
+#include <string>
+
+std::string GetStdoutFromCommand(std::string cmd);
+
+#endif MINIMIZE_MAIN_H
